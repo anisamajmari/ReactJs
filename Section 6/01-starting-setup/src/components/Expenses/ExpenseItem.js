a954700b4a983f1f39e53b070850912e803d41ab
Anisa Majmari <anisamajmari@gmail.com>
2023-09-27 16:08:39 +0200

    using stateful list

--- Section 6/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ Section 6/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -5,20 +5,20 @@ import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandle = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
+  // const clickHandle = () => {
+  //   setTitle("Updated!");
+  //   console.log(title);
+  // };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandle}>Change title</button>
+      {/* <button onClick={clickHandle}>Change title</button> */}
     </Card>
   );
 }
