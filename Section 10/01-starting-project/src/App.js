cb1d5e70cfffed6f1b9d93c552dd7dc6e77a758e
Anisa Majmari <anisamajmari@gmail.com>
2023-10-02 10:12:00 +0200

    React Fragments

--- Section 10/01-starting-project/src/App.js
+++ Section 10/01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
@@ -16,10 +16,20 @@ function App() {
   };
 
   return (
-    <div>
+    // <>
+    //   <AddUser onAddUser={addUserHandler} />
+    //   <UsersList users={usersList} />
+    // </>
+
+    // <React.Fragment>
+    //   <AddUser onAddUser={addUserHandler} />
+    //   <UsersList users={usersList} />
+    // </React.Fragment>
+
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+   </Fragment>
   );
 }
 
