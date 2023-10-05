5d742b2137bff6a98e7aaa68004d171998d1cd22
Anisa Majmari <anisamajmari@gmail.com>
2023-10-05 11:20:59 +0200

    GET request using async/await

--- Section 15/01-starting-project/src/App.js
+++ Section 15/01-starting-project/src/App.js
@@ -6,24 +6,40 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseData: movieData.release_data,
-          };
-        });
-        setMovies(transformedMovies);
-      });
-  }
+  // GET request
+  // function fetchMoviesHandler() {
+  //   fetch("https://swapi.dev/api/films/")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const transformedMovies = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           releaseData: movieData.release_data,
+  //         };
+  //       });
+  //       setMovies(transformedMovies);
+  //     });
+  // }
 
+  // GET request using async/await
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseData: movieData.release_data,
+      };
+    });
+    setMovies(transformedMovies);
+  }
   return (
     <React.Fragment>
       <section>
