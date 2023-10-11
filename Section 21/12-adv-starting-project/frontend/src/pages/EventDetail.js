9bdcee200336e716da1c2320ccfb38b61aee2361
Anisa Majmari <anisamajmari@gmail.com>
2023-10-11 10:31:02 +0200

    deferring data fetching with defer()

--- Section 21/12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ Section 21/12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -11,7 +11,7 @@ import EventItem from "../components/EventItem";
 import EventsList from "../components/EventsList";
 
 function EventDetailPage() {
-  const { event, events } = useRouteLoaderData("event-detail"); // it takes an id as argument
+  const { event, events } = useRouteLoaderData("event-detail");
 
   return (
     <>
@@ -20,10 +20,9 @@ function EventDetailPage() {
           {(loadedEvent) => <EventItem event={loadedEvent} />}
         </Await>
       </Suspense>
-
       <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
         <Await resolve={events}>
-          {(loadedEvents) => <EventsList events={loadEvents} />}
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
         </Await>
       </Suspense>
     </>
@@ -34,6 +33,7 @@ export default EventDetailPage;
 
 async function loadEvent(id) {
   const response = await fetch("http://localhost:8080/events/" + id);
+
   if (!response.ok) {
     throw json(
       { message: "Could not fetch details for selected event." },
@@ -51,13 +51,16 @@ async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // return { isError: true, message: "Could not fetch events." };
-
-    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
     //   status: 500,
     // });
-
-    return json({ message: "Could not fetch events." }, { status: 500 });
+    throw json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
   } else {
     const resData = await response.json();
     return resData.events;
@@ -74,9 +77,9 @@ export async function loader({ request, params }) {
 }
 
 export async function action({ params, request }) {
-  const eventId = params.eventsId;
+  const eventId = params.eventId;
   const response = await fetch("http://localhost:8080/events/" + eventId, {
-    method: request.method, // method: 'DELETE'
+    method: request.method,
   });
 
   if (!response.ok) {
