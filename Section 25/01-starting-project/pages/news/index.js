e0b5d296292e223f7a8f0a281e1298bd6a931b54
Anisa Majmari <anisamajmari@gmail.com>
2023-10-13 16:37:25 +0200

    linking between pages

--- Section 25/01-starting-project/pages/news/index.js
+++ Section 25/01-starting-project/pages/news/index.js
@@ -1,7 +1,19 @@
 // our-domain.com/news
+import Link from "next/link";
+import { Fragment } from "react";
 
 function NewsPage() {
-  return <h1>The News page</h1>;
+  return (
+    <Fragment>
+      <h1>The News page</h1>
+      <ul>
+        <li>
+          <Link href="/news/next-js">NextJS is a great Framework.</Link>
+        </li>
+        <li>Something else.</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
