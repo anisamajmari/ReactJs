79941adeaa6ab192c9967b730efa6e58531f0f4d
Anisa Majmari <anisamajmari@gmail.com>
2023-10-18 11:58:59 +0200

    data fetching for static pages

--- Section 25/06-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ Section 25/06-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -17,8 +18,22 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, []);
+
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
 
 export default HomePage;
