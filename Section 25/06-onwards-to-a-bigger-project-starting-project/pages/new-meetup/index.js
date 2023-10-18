53ebce62693a24364b1eb0f0a67492d7721049a9
Anisa Majmari <anisamajmari@gmail.com>
2023-10-18 09:47:21 +0200

    new-meetup data

--- Section 25/06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
+++ Section 25/06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
@@ -1,1 +1,12 @@
 // our-domain.com/new-meetup
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  function addMeetupHandler(enteredMeetupdata) {
+    console.log(enteredMeetupdata);
+  }
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
