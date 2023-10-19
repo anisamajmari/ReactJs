95030ab65b5c6dc39583f5d24294ce852118ae1e
Anisa Majmari <anisamajmari@gmail.com>
2023-10-19 12:12:33 +0200

    the CSSTransition Component

--- Section 26/animations--04-react-transition-basic/src/components/Modal/Modal.js
+++ Section 26/animations--04-react-transition-basic/src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Transition from "react-transition-group/Transition";
+// import { CSSTransition } from "react-transition-group/CSSTransition";
 
 import "./Modal.css";
 
@@ -19,6 +20,26 @@ const modal = (props) => {
   // ];
 
   return (
+    // <CSSTransition
+    //   mountOnEnter
+    //   unmountOnExit
+    //   in={props.show}
+    //   timeout={animationTiming}
+    //   classNames={{
+    //     enter: "",
+    //     enterActive: "ModalOpen",
+    //     exit: "",
+    //     exitActive: "ModalClosed",
+    //   }}
+    // >
+    //   <div className="Modal">
+    //     <h1>A Modal</h1>
+    //     <button className="Button" onClick={props.closed}>
+    //       Dismiss
+    //     </button>
+    //   </div>
+    // </CSSTransition>
+
     <Transition
       mountOnEnter
       unmountOnExit
