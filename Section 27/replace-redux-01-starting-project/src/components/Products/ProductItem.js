4d1c1e97f18466cddae07552041f62836c4f67ab
Anisa Majmari <anisamajmari@gmail.com>
2023-10-19 15:55:58 +0200

    optimizing the custom hook

--- Section 27/replace-redux-01-starting-project/src/components/Products/ProductItem.js
+++ Section 27/replace-redux-01-starting-project/src/components/Products/ProductItem.js
@@ -4,11 +4,10 @@ import Card from "../UI/Card";
 import { useStore } from "../../hooks-store/store";
 import "./ProductItem.css";
 
-const ProductItem = (props) => {
-  const dispatch = useStore()[1];
+const ProductItem = React.memo((props) => {
+  const dispatch = useStore(false)[1];
 
   const toggleFavHandler = () => {
-    // toggleFav(props.id);
     dispatch("TOGGLE_FAV", props.id);
   };
 
@@ -26,6 +25,6 @@ const ProductItem = (props) => {
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
