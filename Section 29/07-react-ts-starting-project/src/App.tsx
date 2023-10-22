65815993857f108e272602873ea119d906896048
Anisa Majmari <anisamajmari@gmail.com>
2023-10-22 13:26:32 +0200

    working with "function props"

--- Section 29/07-react-ts-starting-project/src/App.tsx
+++ Section 29/07-react-ts-starting-project/src/App.tsx
@@ -1,11 +1,15 @@
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
   const todos = [new Todo("React"), new Todo("TypeScript")];
 
+  const addTodoHandler = (text: string) => {};
+
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
