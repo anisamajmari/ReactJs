83412c6beabdad8deb02595ca053532bbbdc4140
Anisa Majmari <anisamajmari@gmail.com>
2023-10-22 14:37:42 +0200

    working with MongoDB

--- Section 25/06-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
+++ Section 25/06-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
@@ -1,8 +1,23 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://Anisa:anisa123@atlascluster.ohxevoj.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
