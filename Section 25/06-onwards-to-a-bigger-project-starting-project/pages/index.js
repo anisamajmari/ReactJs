3b747b62698c0cadfd5ec734fe0fdc77fe899714
Anisa Majmari <anisamajmari@gmail.com>
2023-10-22 17:25:48 +0200

    getting data from database

--- Section 25/06-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ Section 25/06-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,31 +1,34 @@
-import { useEffect, useState } from "react";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A first Meetup",
-    image: "https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg",
-    address: "Some address 3, 5544 , City",
-    description: "This is a first meetup.",
-  },
-  {
-    id: "m2",
-    title: "A second Meetup",
-    image: "https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg",
-    address: "Some address 5, 24525 , City",
-    description: "This is a second meetup.",
-  },
-];
+// const DUMMY_MEETUPS = [
+//   {
+//     id: "m1",
+//     title: "A first Meetup",
+//     image: "https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg",
+//     address: "Some address 3, 5544 , City",
+//     description: "This is a first meetup.",
+//   },
+//   {
+//     id: "m2",
+//     title: "A second Meetup",
+//     image: "https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg",
+//     address: "Some address 5, 24525 , City",
+//     description: "This is a second meetup.",
+//   },
+// ];
 
 function HomePage(props) {
-  const [loadedMeetups, setLoadedMeetups] = useState([]);
+  // const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  useEffect(() => {
-    setLoadedMeetups(DUMMY_MEETUPS);
-  }, []);
+  // useEffect(() => {
+  //   setLoadedMeetups(DUMMY_MEETUPS);
+  // }, []);
 
-  return <MeetupList meetups={loadedMeetups} />;
+  return <MeetupList meetups={props.meetups} />;
 }
 
 // export async function getServerSideProps(context) {
@@ -37,13 +40,26 @@ function HomePage(props) {
 //   };
 // }
 
-// export async function getStaticProps() {
-//   return {
-//     props: {
-//       meetups: DUMMY_MEETUPS,
-//     },
-//     revalidate: 1,
-//   };
-// }
+export async function getStaticProps() {
+  const client = await MongoClient.connect(process.env.MONGO_URI);
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
 
 export default HomePage;
